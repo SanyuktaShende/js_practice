9c5379a369ab2b787039c5bf253334070c621058
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-04-11 20:20:39 +0530

    operators

---
+++ primitiveDataTypes.js
@@ -0,0 +1,181 @@
+
+
+```
+let integerNumber = 3;
+console.log(typeof(integerNumber));    // number
+
+let floatNumber = 3.14;
+console.log(typeof(floatNumber));       //number
+
+let output = integerNumber + floatNumber;
+// Result will be a floating-point number
+
+console.log(output);    // 6.14
+```
+```
+let number1 = 13;
+let number2 = 10;
+
+let sum = number1 + number2;
+console.log(sum);                     // 23
+
+let product = number1 * number2;
+console.log(product);                // 103
+
+let quotient = number1/number2;
+console.log(quotient);               // 1.3
+```
+
+```
+let strNum = "25";
+console.log(typeof(strNum));           // string
+
+let strFloat = "35.63";
+console.log(typeof(strFloat));         // string
+
+let parsedInteger = parseInt(strNum);
+let parsedFloat = parseInt(strFloat);
+
+console.log(typeof(parsedInteger));   // number
+console.log(typeof(parsedFloat));     // number
+```
+
+```
+let string1 =  "Namaste";
+console.log(typeof(string1));      // string
+
+let string2 = 'Namaste';
+console.log(typeof(string2));      // string
+
+let string3 - `Namaste`;
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+```
+let string1 = "Mobile";
+
+let string2 = "Charger";
+
+let finalSentence = string1 + " and " + string2 + "!";
+
+
+console.log(finalSentence);
+
+Output : Mobile and Charger!
+
+```
