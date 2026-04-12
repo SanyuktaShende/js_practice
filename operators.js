29ff43f5d1a10f417692594fbbcaae008d31aca8
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-04-12 14:07:52 +0530

    operators modiied

--- operators.js
+++ operators.js
@@ -110,3 +110,60 @@ console.log(x == y);            // true
 console.log(x === y);           // false
 ```
 
+//AND operator
+```
+let alert1 = true;
+let alert2 = false;
+let alert3 = true;
+
+console.log(alert1 && alert2);    // false
+console.log(alert2 && alert3);    // false
+console.log(alert1 && alert3);    // true
+
+let num1 = 0;
+let num2 = 1;
+let num3 = 1;
+
+console.log(num1 && num2);    // 0
+console.log(num2 && num3);    // 1
+console.log(num1 && num3);    // 0
+```
+
+//OR operator
+```
+let alert1 = false;
+let alert2 = false;
+let alert3 = true;
+
+console.log(alert1 || alert2);    // false
+console.log(alert2 || alert3);    // true
+console.log(alert1 || alert3);    // true
+
+let num1 = 0;
+let num2 = 1;
+let num3 = 0;
+
+console.log(num1 || num2);    // 1
+console.log(num2 || num3);    // 1
+console.log(num1 || num3);    // 0
+```
+
+//NOT operator
+```
+let alert1 = false;
+let alert2;
+let alert3 = NaN;
+let mySum = 45;
+let gone = false;
+let isFirst = true;
+
+console.log(!alert1);      // true
+console.log(!alert2);      // true
+console.log(!alert3);      // true
+console.log(!mySum);       // false
+console.log(!gone);        // true
+console.log(!isFirst);     // false
+```
+
+
+
