d91eba83ee8549f0e9b69fadb03ce2ec8198a36d
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-04-12 13:45:01 +0530

    operators

--- operators.js
+++ operators.js
@@ -2,3 +2,111 @@ let x = 2;
 let y = 4;
 let sum = x+y;
 console.log(sum);
+
+```
+let number1 = 25;
+let number2 = 36;
+let number3 = 56;
+
+let sum = number1 + number2 + number3;
+console.log(sum);    // 117
+// ```
+
+```
+let number1 = 25;
+let number2 = 36;
+
+let diff = number1 - number2;
+console.log(diff);    // -11
+```
+
+```
+let number1 = 25;
+let number2 = 36;
+
+let prod = number1 * number2;
+console.log(prod);    // 900
+```
+
+```
+let x = 5;
+let y = 5;
+
+let ans = x ** 3;
+console.log(ans);      // 125
+
+ans = x ** y;
+console.log(ans);      // 3125
+```
+
+```
+let x = 10;
+let y = 4;
+
+let ans = x/y;
+console.log(ans);    // 2.5
+```
+
+```
+let x = 10;
+let y = 4;
+
+let ans = x % y;
+console.log(ans);    // 2
+```
+
+```
+let x = 10;
+let y = 4;
+
+x++;
+console.log(x);      // 11
+
+y--;
+console.log(y);      // 3
+```
+
+//Post-increment/decrement 
+
+```
+//Pre increment/decrement
+let x = 10;
+let y = 4;
+
+console.log(++x);    // 11
+console.log(--y);    // 3
+console.log(x);      // 11
+console.log(y);      // 3
+
+//Post increment/decrement
+let a = 10;
+let b = 4;
+console.log(a++);      // 10
+console.log(b--);      // 4
+
+console.log(a);        // 11
+console.log(b);        // 3
+```
+
+```
+let firstName = "Mayank";
+let y = 4;
+
+console.log(firstName);      // Mayank
+console.log(y);              // 4
+```
+
+//Strict Equality v/s Loose Equality
+
+```
+let x = "10";
+console.log(typeof(x));        // string
+
+let y = 10;
+console.log(typeof(y));         // number
+
+console.log(x == y);            // true
+
+console.log(x === y);           // false
+```
+
