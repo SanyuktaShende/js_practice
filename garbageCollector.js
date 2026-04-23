63e5e33823f0f4e025b5a944e456b7f3d6ce98c0
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-04-23 22:32:21 +0530

    gc

---
+++ garbageCollector.js
@@ -0,0 +1,14 @@
+
+```
+
+What is Garbage Collection?
+Garbage Collector = automatically removes unused memory
+Garbage collection is an automatic memory management process that identifies and deallocates memory occupied by objects that are no longer needed by the program. In JavaScript, this process is performed by the runtime environment, typically a web browser or Node.js, 
+rather than the programmer manually freeing memory.
+
+
+EG:
+let user = { name: "Sanyukta" };
+
+user = null; // remove reference
+
