dc3fd92ed2169a0ed724c51eb77143db88d777af
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-05-09 17:39:23 +0530

    all about events

---
+++ eventHandlersTheory.js
@@ -0,0 +1,43 @@
+```
+
+What are 'Events'?
+Events are occurrences that happen in the browser environment triggered by user actions or system-generated notifications.
+
+Types of Events
+JavaScript supports a wide range of events, categorized into different types such as Mouse Events, Keyboard Events, Form Events, Document/Window Events, and Custom Events. 
+
+
+Mouse Events
+click: The event occurs when the user clicks on an element.
+mouseover: The event occurs when the mouse pointer is moved onto an element.
+mouseout: The event occurs when the mouse pointer is moved out of an element.
+mousemove: The event occurs when the mouse pointer is moving while it is over an element.
+
+Keyboard Events
+keydown: The event occurs when the user is pressing a key down.
+keyup: The event occurs when the user releases a key.
+keypress: The event occurs when a key is pressed down and then released.
+
+
+Form Events
+submit: The event occurs when a form is submitted.
+change: The event occurs when the value of an element changes (for input, textarea, and select elements).
+input: The event occurs when an input element gets user input.
+
+
+Window Events
+load: The event occurs when the browser has finished loading all the resources of the document.
+resize: The event occurs when the browser window changes size.
+scroll: The event occurs when the document view is scrolled.
+
+Media Events
+play: The event occurs when the audio/video has been started or is no longer paused.
+pause: The event occurs when the audio/video has been paused.
+
+
+Drag and Drop Events
+dragstart: The event occurs when the user starts to drag an element.
+dragend: The event occurs when the user has finished dragging the element.
+
+----------------------------------------------------------------------------------------------------------------
+
