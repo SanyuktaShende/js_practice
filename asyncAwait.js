ccf2c9059296059f05612b490785adf6a432fd5b
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-05-10 13:56:11 +0530

    asyncAwait

---
+++ asyncAwait.js
@@ -0,0 +1,19 @@
+
+/**
+ Understanding setTimeout()
+ The setTimeout() method is a built-in JavaScript function that allows us to schedule the execution of a function or the evaluation of an expression after a specified delay, measured in milliseconds.
+ 
+ setTimeout(function, delay);
+ 
+ function sayHello() {
+   console.log("Hello after 2 seconds!");
+ }
+
+ setTimeout(sayHello, 2000); // Call sayHello after 2 seconds (2000 milliseconds)
+
+ console.log("This will be printed immediately.");
+ 
+ 
+ */
+ 
+
